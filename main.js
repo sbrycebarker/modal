613b42b58dde6154f630f7baef2922489fe0cfaa
sbrycebarker <sbrycesti@gmail.com>
2018-02-15 13:28:38 -0700

    Merge branch 'master' of https://github.com/sbrycebarker/modal

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
+// Get modal element
 
+var modal = document.getElementById('simpleModal')
+
+var modalbtn = document.getElementById('modalbtn')
+
+var closebtn= document.getElementById('closebtn');
+
+modalbtn.addEventListener('click', openModal);
+
+closebtn.addEventListener('click', closeModal);
+
+window.addEventListener('click', outside)
+
+function openModal() {
+  modal.style.display = 'block'
+}
+
+function closeModal() {
+  modal.style.display = 'none';
+}
+
+function outside(e) {
+  if(e.target == modal) {
+    modal.style.display = "none"
+  }
+}
