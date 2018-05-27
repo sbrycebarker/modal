fd9e443b2eeff8297c6be91f705ae1c2bb384a29
sbrycebarker <sbrycesti@gmail.com>
2018-05-26 19:13:03 -0600

    modal fixes

--- main.js
+++ main.js
@@ -1,27 +1,30 @@
 // Get modal element
+var modal = document.getElementById('simpleModal');
+// Get open modal button
+var modalBtn = document.getElementById('modalBtn');
+// Get close button
+var closeBtn = document.getElementsByClassName('closeBtn')[0];
 
-var modal = document.getElementById('simpleModal')
+// Listen for open click
+modalBtn.addEventListener('click', openModal);
+// Listen for close click
+closeBtn.addEventListener('click', closeModal);
+// Listen for outside click
+window.addEventListener('click', outsideClick);
 
-var modalbtn = document.getElementById('modalbtn')
-
-var closebtn= document.getElementById('closebtn');
-
-modalbtn.addEventListener('click', openModal);
-
-closebtn.addEventListener('click', closeModal);
-
-window.addEventListener('click', outside)
-
-function openModal() {
-  modal.style.display = 'block'
+// Function to open modal
+function openModal(){
+  modal.style.display = 'block';
 }
 
-function closeModal() {
+// Function to close modal
+function closeModal(){
   modal.style.display = 'none';
 }
 
-function outside(e) {
-  if(e.target == modal) {
-    modal.style.display = "none"
+// Function to close modal if outside click
+function outsideClick(e){
+  if(e.target == modal){
+    modal.style.display = 'none';
   }
 }
